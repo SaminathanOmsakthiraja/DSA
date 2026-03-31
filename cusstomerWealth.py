3826b26d5009d2de34358994fab5110a83440620
OM.Saminathan <omsaminathan@gmail.com>
2026-03-31 22:41:09 +0530

    Delete cusstomerWealth.py

--- cusstomerWealth.py
+++
@@ -1,9 +0,0 @@
-def MaximumWealth(account):
-    max = 0
-    for customer in account:
-        wealth = sum(customer)
-        if wealth > max:
-            max = wealth
-    return max
-account = [[1,2,3],[3,2,1]]
-print(MaximumWealth(account))
