875335ad33dd3acdc386d7f5e993595397d61b2f
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-04-01 11:37:37 +0530

    Find the missing numbers in continous array

---
+++ MissingnuminArray.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MissingnuminArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        List<Integer> result =  new ArrayList<>();
+        for (int i = 0; i < n ; i++){
+            boolean found = false;
+            for (int j = 0; j < arr.length; j++) {
+                if ((i+1)==arr[j]){
+                    found = true;
+                }
+            }
+            if (!found)
+                result.add(i+1);
+        }
+        System.out.println(result);
+        sc.close();
+    }
+    
+}
