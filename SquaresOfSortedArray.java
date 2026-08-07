b1ed92cdfa4ffa688738109c9dc00bd085a508e1
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-08-07 15:15:39 +0530

    Squares of the Sorted Array

---
+++ SquaresOfSortedArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length; i++)
+            nums[i] *= nums[i];
+        Arrays.sort(nums);
+        return nums;
+    }
+}
