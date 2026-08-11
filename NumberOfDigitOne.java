c7248003892accea2c52d2afc45e4910e529d8fd
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-08-11 11:13:57 +0530

    Its is bruetforce method it donot solve some edge cases

---
+++ NumberOfDigitOne.java
@@ -0,0 +1,18 @@
+class NumberOfDigitOne {
+    public static int countDigitOne(int n) {
+        int count = 0;
+        if (n == 1)
+            return 1;
+        for (int i = n; i > 0; i--){
+            String s =  Integer.toString(i);
+            for (int c = 0; c < s.length(); c++){
+                if (s.charAt(c) == '1')
+                    count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countDigitOne(100));
+    }
+}
