99921774fbab62ebb3260fbda431486ad3fd8814
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-08-30 21:38:05 +0530

    You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
    A triangle is called equilateral if it has all sides of equal length.
    A triangle is called isosceles if it has exactly two sides of equal length.
    A triangle is called scalene if all its sides are of different lengths.
    Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.

---
+++ TypeOfTriangle.java
@@ -0,0 +1,13 @@
+public class TypeOfTriangle {
+    public String triangleType(int[] nums) {
+        if (nums[0] >= (nums[1]+nums[2]) || nums[2] >= (nums[1]+nums[0]) || nums[1] >= (nums[0]+nums[2]))
+            return "none";
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+            return "equilateral";
+        else if (nums[0] != nums[1] && nums[1] != nums[2] && nums[2] != nums[0])
+            return "scalene";
+        else if ((nums[0] == nums[1] && nums[1] != nums[2]) || (nums[0] != nums[1] && nums[1] == nums[2]) || (nums[2] == nums[0] && nums[2] != nums[1]))
+            return "isosceles";
+        return "none";
+    }
+}
