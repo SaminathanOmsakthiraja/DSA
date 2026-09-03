14bf48aea6329c130b5fbb818c67a6a8fb686ec2
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-09-03 22:47:42 +0530

    Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.

---
+++ ThreeConsecutiveOddNumbers.java
@@ -0,0 +1,16 @@
+public class ThreeConsecutiveOddNumbers {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int i = 0 ; i < arr.length ; i++){
+            if (count == 3)
+                return true;
+            if  (arr[i] % 2 != 0)
+                count++;
+            else
+                count = 0;
+        }
+        if (count == 3)
+                return true;
+        return false;
+    }
+}
